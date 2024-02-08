45f96f0945057e34a36d8a0af3531ed616140ed5
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-02-07 23:08:39 -0500

    Seekbars move as intended

--- app/src/main/java/com/example/matthewhilliard_temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/matthewhilliard_temperatureconverter/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.matthewhilliard_temperatureconverter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +13,35 @@ class MainActivity : AppCompatActivity() {
         val celsiusSeekbar = findViewById<SeekBar>(R.id.celsiusSeekBar)
         val fahrenheitSeekbar = findViewById<SeekBar>(R.id.fahrenheitSeekBar)
 
+        val celsiusReading = findViewById<TextView>(R.id.celsiusReading)
+        val fahrenheitReading = findViewById<TextView>(R.id.fahrenheitReading)
+
+        celsiusSeekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                celsiusReading.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        fahrenheitSeekbar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                fahrenheitReading.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
     }
 }
