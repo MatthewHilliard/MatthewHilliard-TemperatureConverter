364ebb3c7abf06403d27e9b80811084c9ac1599c
MatthewHilliard <matthewhilliard2003@gmail.com>
2024-02-07 19:30:51 -0500

    Created seekbar variables in main activity

--- app/src/main/java/com/example/matthewhilliard_temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/example/matthewhilliard_temperatureconverter/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.matthewhilliard_temperatureconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val celsiusSeekbar = findViewById<SeekBar>(R.id.celsiusSeekBar)
+        val fahrenheitSeekbar = findViewById<SeekBar>(R.id.fahrenheitSeekBar)
+
     }
 }
